d738624cadd3296f11d2cbdf5971da0470983d20
Mateusz Łaś <mateuszlas3@gmail.com>
2024-03-25 12:28:17 +0100

    init

---
+++ LogcatHttpClient/Tools/PingTool.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.NetworkInformation;
+
+namespace LogcatHttpClient.Tools;
+
+public class PingTool
+{ 
+    public string Ping(string url)
+    {
+        Ping ping = new();
+        try
+        {
+            PingReply reply = ping.Send(url, 3000);
+            if (reply.Status == IPStatus.Success)
+            {
+                 return $"Ping successful.";
+            }
+            else
+            {
+                return $"Ping failed: {reply.Status.ToString()}";
+            }
+        }
+        catch (PingException ex)
+        {
+            return $"Ping failed: {ex.Message}";
+        }
+    }
+}
